6ab7efe2b16780b72d73b91fe31c7db364d4d9a0
Kim Young Jin <with.alpha.and.omega@gmail.com>
2019-07-14 00:06:30 +0900

    feature: Add Markup
    생성된 단축 URL 목록을 조회하기 위한 엔드포인트 추가
    단축 URL 생성을 위한 마크업 추가
      * 메인 페이지
      * 404 Not found 페이지
      * 단축 URL 목록 페이지

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from compressor.views import IndexView, RedirectView
+from compressor.views import IndexView, RedirectView, UrlListView
 
 
 app = Flask(__name__)
@@ -17,6 +17,10 @@ app.add_url_rule(
 app.add_url_rule(
     rule='/<url>',
     view_func=RedirectView.as_view('redirect')
+)
+app.add_url_rule(
+    rule='/list',
+    view_func=UrlListView.as_view('list')
 )
 
 
